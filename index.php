5a201bb25c1562c4dcf7090d710e77721bf1cd9c
=MaiwL <Maiw.Lme@hotmail.fr>
2023-06-14 16:17:40 +0200

    Ajout

--- index.php
+++ index.php
@@ -1,4 +1,43 @@
 <?php ob_start() ?> 
+<!--  -->
+<div class="position-relative pb-5">
+    <img src="./Images/Bibliotheque-header.jpg" class="w-100 h-75 img-fluid" alt="">
+     <p class="display-1 position-absolute top-50 start-50 translate-middle" style= "color : #E8C5B4; font-family: 'Playfair Display', serif;">Bienvenue</p>
+</div>
+
+<div class=" couleur-bordure container border-top border-2 mt-5"></div>
+
+<p class="container p-5">La bibliothèque de Dunkerque vous ouvre ses portes. Ouvertes il y a quelques mois, vous y trouveraient bon nombre d'ouvrages, vous pourriez lire vos livres sur place ou bien les emprunter pour les lires dans votre endroit préféré !</p>
+
+<div class=" couleur-bordure container border-top border-2 mt-5"></div>
+
+<div class="container">
+    <div class="row justify-content-center gap-5 mt-5">
+      <div class=" px-0 mb-5" style="width: 18rem;">
+        <img src="./Images/Lire.jpg" class="card-img-top rounded-0" alt="Robot">
+        <div class="card-body">
+          <p class="card-text">Venez découvrir les nouveautés de la semaine...</p>
+          <a href="#" class="btn" style=" color:#E8C5B4;">Nouveautés</a>
+        </div>
+      </div>
+      <div class=" px-0 mb-5" style="width: 18rem;">
+        <img src="./Images/Enfants.jpg" class="card-img-top rounded-0" alt="PC">
+        <div class="card-body">
+          <p class="card-text">Venez découvrir nos différents coups de coeur !</p>
+          <a href="#" class="btn" style=" color:#E8C5B4;">Coups de coeur</a>
+        </div>
+      </div>
+      <div class=" px-0 mb-5" style="width: 18rem;">
+        <img src="./Images/Livres.jpg" class="card-img-top rounded-0" style="height:193px;" alt="Page de code">
+        <div class="card-body">
+          <p class="card-text">Pour plus d'informations veuillez cliquer en dessous !</p>
+          <a href="#" class="btn" style="color:#E8C5B4;">Horaires et informations</a>
+        </div>
+      </div>
+    </div>
+    
+</div>
+
 
 <?php
 $content =ob_get_clean();
--- template.php
+++ template.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="fr">
+<html class="perso" lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,23 +8,29 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
+<body class="perso" style="font-family:'Montserrat Alternates', sans-serif; ">
     <header>
         <nav class="navbar navbar-light" style="background-color : #E8C5B4 ;">
             <div class="container-fluid">
-                <a class="navbar-brand" style="color: #CF756C;">Books</a>
-                <a href="/index.php" class="nav-link" style="color : #CF756C;">Accueil</a>
-                <a href="#" class="nav-link" style="color : #CF756C;"></a>
+                <div><a class="navbar-brand" style="color: #CF756C;">Books</a></div>
+                <div><a href="/index.php" class=" nav-link" style="color : #CF756C;">Accueil</a></div>
+                <div><a href="#" class="nav-link" style="color : #CF756C;">Nouveautés</a></div>
+                <div><a href="#" class="nav-link" style="color : #CF756C;">Librairie</a></div>
                 <form class="d-flex">
                     <input class="form-control me-2" type="search" aria-label="Search">
                     <button class="btn" style="border-color : #CF756C; color : #CF756C; "type="submit">Recherche</button>
-                    <a href="#" ></a>
+                    <a href="#"><img src="./Images/R.png" style="width:30px; height:30px;" class="ms-2 mt-1" alt=""></a>
+                    
                 </form>
             </div>
-        </nav>
-        <img src="/Images/Bibliothèque-header.jpg" class="w-100 h-75" alt="">
-        <p>Bienvenue</p>
+        </nav>   
     </header>
+
+
+
+
+
+
     
 
 
@@ -32,6 +38,20 @@
 
 
     <?= $content ?>
+
+    
+    <footer >
+            <div  class=" couleur-bordure container border-top border-2">
+            </div>
+            <div class="container d-flex justify-content-center p-5">
+               <a href="#">Mon compte </a><span>|</span>
+               <a href="#">Mentions légales</a><span> | </span>
+               <a href="#">Contact</a>
+            </div>
+            <div class="" style="background-color : #E8C5B4;">
+                <p class=" perso text-center p-3 ">Développeur : Jayson Brement | Maïwenn Lemaire</p>
+            </div>
+    </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
